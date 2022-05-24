71b17592f96ff4a504d48f890ba0ec04d9c69be8
Nathan Scott <nathanscott@nathans-mbp.lan>
2022-05-24 12:37:23 -0500

    range

--- problems/04-range.js
+++ problems/04-range.js
@@ -10,14 +10,14 @@ range(3, 4); // [3]
 range(7, 6); // []
 ***********************************************************************/
 function range(start, end) {
-  let arr = [];
-  if (end < start) {
+  if (end <= start) {
     return [];
   }
-  return arr.push([start, ...range(start + 1, end)])
+
+  return [start, ...range(start + 1, end)]
 }
 
-
+console.log(range(1, 5)); // [1, 2, 3, 4])
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
