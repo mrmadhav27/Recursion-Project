c7e6e1f0ddb32a7d7cc218fd029cf2543cc9b04f
Justin <mancussion@gmail.com>
2022-05-24 17:56:41 -0400

    12 Solved

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -11,6 +11,39 @@ isSorted([5, 4, 3, 2, 1]); // false
 ***********************************************************************/
 
 // your code here
+// const isSorted = (arr) => {
+//   if (arr.length === 1) return true
+//   if (arr[0] > arr[1]) return false
+//   arr.shift()
+//   return isSorted(arr)
+// }
+
+// const isSorted = (arr) => {
+//   if (arr.length === 1) return true
+//   if (arr.pop() > arr[arr.length - 1]) {
+//     return isSorted(arr)
+//   } else return false
+
+// }
+
+// const isSorted = (arr) => {
+//   if (arr.length === 1) return true
+//   if (arr.shift() < arr[0]) {
+//     return isSorted(arr)
+//   } else return false
+// }
+
+const isSorted = nums => {
+  if (nums.length === 1) return true
+  if (nums[0] > nums[1]) return false
+  let newNum = nums.slice(1)
+  return isSorted(newNum)
+}
+
+// console.log(isSorted([1, 2, 3, 4, 5])); // true
+// console.log(isSorted([1, 2, 4, 3, 5])); // false
+// console.log(isSorted([2, 4, 6, 7, 8])); // true
+// console.log(isSorted([5, 4, 3, 2, 1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
