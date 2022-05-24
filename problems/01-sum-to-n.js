fe55d2dd031d4dc806833b2807025d27eb23d928
Madhav Sharma <mrmadhav27@gmail.com>
2022-05-24 12:35:09 -0400

    first prob solved

--- problems/01-sum-to-n.js
+++ problems/01-sum-to-n.js
@@ -11,7 +11,12 @@ sumToN(9)  // returns 45
 sumToN(-8)  // returns null
 ***********************************************************************/
 
-// your code here
+const sumToN = (num) => {
+  if (num < 0) return null;
+  if (num === 0) return num;
+
+  return num + sumToN(num - 1);
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
