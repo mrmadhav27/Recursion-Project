c440c1f4ed78b121854be268bd3c00d8e72d99b7
Madhav Sharma <mrmadhav27@gmail.com>
2022-05-24 17:30:57 -0400

    11th solved

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -10,8 +10,21 @@ flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
-// your code here
-  
+const flatten = (arr) => {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let ele = arr[i];
+    if (Array.isArray(ele)) {
+      newArr.push(...flatten(ele));
+    } else {
+      newArr.push(ele);
+    }
+  }
+  return newArr;
+}
+
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = flatten;
