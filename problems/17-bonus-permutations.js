2dfbd2dd49309f33a8e67552b9c801d5df9a4787
Justin <mancussion@gmail.com>
2022-05-25 19:06:24 -0400

    17 solved

--- problems/17-bonus-permutations.js
+++ problems/17-bonus-permutations.js
@@ -13,6 +13,26 @@ permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
 ***********************************************************************/
 
 // your code here
+const permutations = nums => {
+  let results = []
+  if (nums.length === 1) return [nums]
+  nums.forEach((num, i) => {
+
+    let copy = nums.slice()
+    // pulls out each num individually
+    copy.splice(i, 1)
+    //
+    let remainingElements = permutations(copy)
+    let firstNum = remainingElements.map(perm => [num, ...perm])
+    results.push(...firstNum)
+  });
+  return results;
+  // console.log(results)
+}
+
+console.log(permutations([1, 2, 3])) // [[1, 2, 3], [1, 3, 2],
+// [2, 1, 3], [2, 3, 1],
+// [3, 1, 2], [3, 2, 1]]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
